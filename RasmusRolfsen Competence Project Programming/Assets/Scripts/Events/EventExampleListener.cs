0dcf8658607c554e565506e0d63173db4ed0c63e
Unknown <rasmus.rolfsen@mail.dk>
2017-10-09 23:48:36 +0200

    event system added

---
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Events/EventExampleListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EventExampleListener : MonoBehaviour
+{
+	[SerializeField]
+	private string logMessage;
+
+	// Remove event when object is destroyed
+	private void OnDestroy()
+	{
+		EventManager.StopListening("DebugEvent", DebugEvent);
+	}
+
+	// Enable the event
+	private void OnEnable()
+	{
+		EventManager.StartListening("DebugEvent", DebugEvent);
+	}
+
+	// Disable the event
+	private void OnDisable()
+	{
+		EventManager.StopListening("DebugEvent", DebugEvent);
+	}
+
+	// Function To Be Executed
+	void DebugEvent()
+	{
+		Debug.Log(logMessage);
+	}
+}
---
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Events/EventExampleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventExampleTrigger : MonoBehaviour
+{
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			EventManager.TriggerEvent("DebugEvent");
+		}
+	}
+}
