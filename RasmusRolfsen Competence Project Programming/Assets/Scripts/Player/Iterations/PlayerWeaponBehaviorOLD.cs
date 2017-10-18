1454c7218be1835de9b7f5abe6a31cc95fb8e280
Unknown <rasmus.rolfsen@mail.dk>
2017-10-18 20:55:47 +0200

    Removed Old script warnings
    a

--- RasmusRolfsen Competence Project Programming/Assets/Scripts/Player/Iterations/PlayerWeaponBehaviorOLD.cs
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Player/Iterations/PlayerWeaponBehaviorOLD.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+#pragma warning disable 0168 // variable declared but not used.
+#pragma warning disable 0219 // variable assigned but not used.
+#pragma warning disable 0414 // private field assigned but not used.
+
 
 
 public class PlayerWeaponBehaviorOLD : MonoBehaviour
