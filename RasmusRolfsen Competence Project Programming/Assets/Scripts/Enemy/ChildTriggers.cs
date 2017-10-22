6835db73b7808c6a6d5b8ae0e5c2f395d8ba6cd5
Unknown <rasmus.rolfsen@mail.dk>
2017-10-23 01:29:51 +0200

    EnemyStates

---
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Enemy/ChildTriggers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildTriggers : MonoBehaviour {
+
+	[SerializeField]
+	EnemyStates.enemyState enterState;
+
+	[SerializeField]
+	EnemyStates.enemyState exitState;
+
+	EnemyStates parentState;
+
+	private void Start()
+	{
+		parentState = GetComponentInParent<EnemyStates>();
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			parentState.state = enterState;
+		}
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			parentState.state = exitState;			
+		}
+	}
+}
---
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Enemy/EnemyStates.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct EnemyAttackTypes
+{
+	public float attackSpeed;
+	public float bulletSpeed;
+	public int damage;
+}
+
+public class EnemyStates : MonoBehaviour
+{
+
+
+	public enum enemyState { idle, patrol, notice, attack, chase, returnToPosition };
+
+	[SerializeField]
+	public enemyState state;
+
+	private Transform player;
+
+	private void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		switch (state)
+		{
+			case (enemyState.idle):
+				IdleBehavior();
+				break;
+			case (enemyState.attack):
+				AttackBehavior();
+				break;
+			case (enemyState.notice):
+				NoticeBehavior();
+				break;
+		}
+	}
+
+	private void IdleBehavior()
+	{
+		//
+	}
+
+	private void AttackBehavior()
+	{
+		//
+	}
+
+	private void NoticeBehavior()
+	{
+		//
+	}
+}
