450ef30eaf2539067e858feda789c2e5d54dfd5b
Unknown <rasmus.rolfsen@mail.dk>
2017-10-25 16:05:40 +0200

    OneTime Trap

--- RasmusRolfsen Competence Project Programming/Assets/Scripts/Traps/ContinuesDamage.cs
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Traps/ContinuesDamage.cs
@@ -7,12 +7,18 @@ public class ContinuesDamage : MonoBehaviour {
 	[SerializeField]
 	private float damageToPlayer;
 
+	[SerializeField]
+	private string particleEvent;
+	[SerializeField]
+	private int particleAmount;
+
 	private void OnTriggerStay(Collider other)
 	{
 		if(other.gameObject.tag == "Player")
 		{
 			float damage = damageToPlayer * Time.deltaTime;
 			EventManager.TriggerEvent("PlayerHealth", -damage, null);
+			EventManager.TriggerEvent(particleEvent,other.transform.position,particleAmount);
 		}
 	}
 }
---
+++ RasmusRolfsen Competence Project Programming/Assets/Scripts/Traps/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+	[SerializeField]
+	private float trapDamage;
+	[SerializeField]
+	private int particleAmount;
+	[SerializeField]
+	private string particleEvent = "";
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			EventManager.TriggerEvent("PlayerHealth", -trapDamage,null);
+			EventManager.TriggerEvent(particleEvent, transform.position, particleAmount);
+			Destroy(gameObject);
+		}
+	}
+}
